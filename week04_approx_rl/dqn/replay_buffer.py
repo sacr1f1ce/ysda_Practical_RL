26807d08d85f185579c8a07c6fdba2ccaf760821
benchMark <nuzhnov.ms@phystech.edu>
2026-04-28 13:31:42 +0000

    some hws

--- week04_approx_rl/dqn/replay_buffer.py
+++ week04_approx_rl/dqn/replay_buffer.py
@@ -35,7 +35,7 @@ class ReplayBuffer(object):
             data = self._storage[i]
             obs_t, action, reward, obs_tp1, done = data
             obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
+            actions.append(np.array(action))
             rewards.append(reward)
             obses_tp1.append(np.array(obs_tp1, copy=False))
             dones.append(done)
